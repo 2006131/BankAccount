7dd24521294456ef85996bbbfba3b89543029c99
2006131 <49693309+2006131@users.noreply.github.com>
2019-04-16 13:00:16 -0500

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -2,7 +2,7 @@
 /**
  * Abstract class BankAccount - write a description of the class here
  *
- * @author Zach
+ * @author Zach and cooper
  * @version (version number or date here)
  */
 public abstract class BankAccount
