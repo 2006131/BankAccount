9bffa0f1451198b7e396ddca092997f86f2a56ee
Zach <2006131@students.ankenyschools.org>
2019-04-16 12:59:41 -0500

    Change

--- BankAccount.java
+++ BankAccount.java
@@ -2,7 +2,7 @@
 /**
  * Abstract class BankAccount - write a description of the class here
  *
- * @author
+ * @author Zach
  * @version (version number or date here)
  */
 public abstract class BankAccount
