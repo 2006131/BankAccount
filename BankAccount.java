f69ddcd9e6b9d18fb7732fbcb9b94124a369a8f7
Zach <2006131@students.ankenyschools.org>
2019-04-16 12:54:35 -0500

    Test

--- BankAccount.java
+++ BankAccount.java
@@ -7,18 +7,7 @@
  */
 public abstract class BankAccount
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private double balance;
+    private int numDeposits;
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
